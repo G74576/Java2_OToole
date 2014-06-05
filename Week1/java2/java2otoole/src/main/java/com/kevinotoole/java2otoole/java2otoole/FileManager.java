18847a5c0b32469766e02ddb31153d0ec526a29d
Kevin <kevinotoole@Kevins-MacBook-Pro.local>
2014-06-04 22:43:39 -0400

    Added Read/Write FileManager class

---
+++ Week1/java2/java2otoole/src/main/java/com/kevinotoole/java2otoole/java2otoole/FileManager.java
@@ -0,0 +1,70 @@
+package com.kevinotoole.java2otoole.java2otoole;
+
+import android.content.Context;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Kevin O'Toole
+ * Java 2 Term 1406
+ * Week 1 Project
+ */
+public class FileManager {
+
+    private static FileManager instance;
+
+    //Constructor:
+    public static FileManager getInstance() {
+        if (instance == null){
+            instance = new FileManager();
+        }
+        return instance;
+    }
+
+    //Writing JSON string to file:
+    public Boolean writeStringToFile (Context context, String filename, String content){
+        Boolean result = false;
+        FileOutputStream fos = null;
+
+        try {
+            fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(content.getBytes());
+            result = true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    //Read JSON String from file:
+    public String readStringFromFile (Context context, String filename){
+        String content = "";
+        FileInputStream fis = null;
+
+        try {
+            fis = context.openFileInput(filename);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            byte[] contentBytes = new byte[1024];
+            int bytesRead = 0;
+            StringBuffer contentBuffer = new StringBuffer();
+
+            while ((bytesRead = bis.read(contentBytes)) != -1){
+                content = new String(contentBytes, 0, bytesRead);
+                contentBuffer.append(content);
+            }
+            content = contentBuffer.toString();
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+}
