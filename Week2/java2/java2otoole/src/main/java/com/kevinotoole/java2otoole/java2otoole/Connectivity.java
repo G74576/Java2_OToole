651c020a49b7a358417c67afafa10f863ad91218
Kevin <kevinotoole@Kevins-MacBook-Pro.local>
2014-06-09 16:26:56 -0400

    Added Week 1 to Week 2 - Added details Layout

---
+++ Week2/java2/java2otoole/src/main/java/com/kevinotoole/java2otoole/java2otoole/Connectivity.java
@@ -0,0 +1,46 @@
+package com.kevinotoole.java2otoole.java2otoole;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * Kevin OToole
+ * Java 2 Term 1406
+ * Week 2 Project
+ */
+public class Connectivity {
+
+    static Boolean connected = false;
+    static String connectionType;
+    static NetworkInfo networkInfo;
+    static String TAG = "NETWORK DATA - CONNECTION";
+
+    //Check the connection of the device:
+    public static void connectionStatus(Context context){
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null){
+            if (networkInfo.isConnected()){
+                connectionType = networkInfo.getTypeName();
+                Log.i(TAG, R.string.connType + " " + networkInfo.getTypeName());
+
+                connected = true;
+            }
+        }
+    }
+
+    //Get the connection type:
+    public static String getConnectionType(Context context){
+        connectionStatus(context);
+        return connectionType;
+    }
+
+    //Get the network status:
+    public static Boolean getNetworkStatus(Context context){
+        connectionStatus(context);
+        return connected;
+    }
+
+}
---
+++ Week2/java2/java2otoole/src/main/java/com/kevinotoole/java2otoole/java2otoole/DetailView.java
@@ -0,0 +1,11 @@
+package com.kevinotoole.java2otoole.java2otoole;
+
+/**
+ * Kevin OToole
+ * Java 2 Term 1406
+ * Week 2 Project
+ */
+public class DetailView {
+
+
+}
