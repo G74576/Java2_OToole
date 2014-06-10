7ee940b77612514804247f5a48b1db0ddf40ecfa
Kevin <kevinotoole@Kevins-MacBook-Pro.local>
2014-06-09 20:47:12 -0400

    Set up detail view to get intent from main
    Set up implicit intent for ACTIONVIEW to instagram site

--- Week2/java2a/java2otoole/src/main/java/com/kevinotoole/java2otoole/java2otoole/DetailView.java
+++ Week2/java2a/java2otoole/src/main/java/com/kevinotoole/java2otoole/java2otoole/DetailView.java
@@ -2,7 +2,10 @@ package com.kevinotoole.java2otoole.java2otoole;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
@@ -25,6 +28,7 @@ public class DetailView extends Activity {
     TextView user_name, full_name;
     Button searchBtn;
     RatingBar ratingBar;
+    Float ratingSelected;
 
 
     @Override
@@ -46,7 +50,7 @@ public class DetailView extends Activity {
         searchImg = intent.getStringExtra("SEARCHIMG_KEY");
         imgLink = intent.getStringExtra("LINK_KEY");
 
-        //Set the TextViews & ImageVies:
+        //Set the TextViews & ImageVies with retrieved data:
         user_name.setText(userName);
         full_name.setText(fullName);
         int loader = R.drawable.ic_launcher;
@@ -57,5 +61,36 @@ public class DetailView extends Activity {
         ImageLoader imageLoader1 = new ImageLoader(getApplicationContext());
         imageLoader1.DisplayImage(searchImgUrl, loader, searchImage);
 
+        //Set implicit intent to view image in Instagram:
+        searchBtn = (Button) findViewById(R.id.button);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent searchLink = new Intent(Intent.ACTION_VIEW, Uri.parse(imgLink));
+                startActivity(searchLink);
+            }
+        });
+
+        //Get the rating for the image via the rating bar:
+        ratingBar = (RatingBar) findViewById(R.id.ratingbar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean user) {
+                ratingSelected = ratingBar.getRating();
+            }
+        });
+    }
+
+    @Override
+    public void finish() {
+        Log.i("DETAILS ACTIVITY", "finish");
+
+        Intent detailsBackIntent = new Intent();
+        detailsBackIntent.putExtra("detailUserName", userName);
+        detailsBackIntent.putExtra("detailRating", ratingSelected);
+
+        setResult(RESULT_OK, detailsBackIntent);
+
+        super.finish();
     }
 }
---
+++ Week2/java2a/java2otoole/src/main/java/com/kevinotoole/java2otoole/java2otoole/FileManager.java
@@ -0,0 +1,89 @@
+package com.kevinotoole.java2otoole.java2otoole;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Kevin OToole
+ * Java 2 Term 1406
+ * Week 2 Project
+ */
+public class FileManager {
+
+    private static FileManager instance;
+
+    private FileManager(){
+
+    }
+
+    //Constructor:
+    public static FileManager getInstance() {
+        if (instance == null){
+            instance = new FileManager();
+        }
+        return instance;
+    }
+
+    //Writing JSON string to file:
+    public Boolean WriteFileToString (Context context, String filename, String content){
+        Boolean result = false;
+        FileOutputStream fos = null;
+
+        try {
+            fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(content.getBytes());
+            result = true;
+            Log.i("WRITE_FILE", "Success");
+        } catch (FileNotFoundException e) {
+            Log.e("WRITE FILE ERROR", "Error writing file");
+        }catch (IOException e){
+            Log.e("WRITE FILE ERROR", "Failure");
+        }
+        return result;
+    }
+
+    //Read JSON String from file:
+    public String readStringFromFile (Context context, String filename){
+        String content = "";
+        FileInputStream fis = null;
+
+        File saveData = MainActivity.mContext.getFileStreamPath(MainActivity.fileName);
+
+        if (saveData.exists()) {
+            try {
+                fis = context.openFileInput(filename);
+                BufferedInputStream bis = new BufferedInputStream(fis);
+                byte[] contentBytes = new byte[1024];
+                int bytesRead = 0;
+                StringBuffer contentBuffer = new StringBuffer();
+
+                while ((bytesRead = bis.read(contentBytes)) != -1) {
+                    content = new String(contentBytes, 0, bytesRead);
+                    contentBuffer.append(content);
+                }
+                content = contentBuffer.toString();
+
+            } catch (Exception e) {
+                Log.e("READ FILE ERROR", "Error reading file");
+                e.printStackTrace();
+                return content;
+            } finally {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("READ FILE ERROR", "Error closing input stream");
+                }
+            }
+        }
+
+        return content;
+    }
+}
